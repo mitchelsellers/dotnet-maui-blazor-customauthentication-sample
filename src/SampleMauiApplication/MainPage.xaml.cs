88786b8f25824da411d27724bb3c9feab32c63a1
Mitchel Sellers <msellers@iowacomputergurus.com>
2022-04-03 22:51:02 -0500

    Default project configuration, no security implementation.

---
+++ src/SampleMauiApplication/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleMauiApplication;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/SampleMauiApplication/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using SampleMauiApplication.Data;
+
+namespace SampleMauiApplication;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.RegisterBlazorMauiWebView()
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddBlazorWebView();
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
