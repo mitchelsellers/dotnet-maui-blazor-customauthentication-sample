1b1a5f4a209df2971f56a7d9facee1776ec7a3c6
GolfJimB <jim_barton@hotmail.co.uk>
2022-06-17 13:07:54 +0800

    Adding social logins

--- src/SampleMauiApplication/MauiProgram.cs
+++ src/SampleMauiApplication/MauiProgram.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebView.Maui;
 using SampleMauiApplication.Data;
+using SampleMauiApplication.Services;
 
 namespace SampleMauiApplication;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
+    public static string APIUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5096/api/" : "https://localhost:44334/api/";
+    public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
@@ -24,7 +26,8 @@ public static class MauiProgram
 
 		builder.Services.AddBlazorWebView();
 		builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddScoped(_ => new AuthenticatingService(APIUrl));
 
-		return builder.Build();
+        return builder.Build();
 	}
 }
--- src/SampleMauiApplication/Models/LoginViewModel.cs
+++ src/SampleMauiApplication/Models/LoginViewModel.cs
@@ -13,38 +13,17 @@ namespace SampleMauiApplication.Models
 
         public bool LoginFailureHidden { get; set; } = true;
 
-        public async Task<bool> ValidateLoginAsync(out string jwtToken)
+        public async Task<string> ValidateLoginAsync()
         {
-            try
-            {
-                WebAuthenticatorResult authResult = await WebAuthenticator.Default.AuthenticateAsync(
-                    new WebAuthenticatorOptions()
-                    {
-                        Url = new Uri("https://mysite.com/mobileauth/Microsoft"),
-                        CallbackUrl = new Uri("myapp://"),
-                        PrefersEphemeralWebBrowserSession = true
-                    });
-
-                string accessToken = authResult?.AccessToken;
-
-                // Do something with the token
-            }
-            catch (TaskCanceledException e)
-            {
-                // Use stopped auth
-            }
-
 
             if (Username.Equals("Test") && Password.Equals("Test"))
             {
-                jwtToken = "123456";
-                return true;
+                return "123456";
             }
 
             //Not valid
-            jwtToken = null;
             LoginFailureHidden = false;
-            return false;
+            return null;
         }
     }
 }
